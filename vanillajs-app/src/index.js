f11ec227f0455815c2990f3888ba6f826d1cf4b5
bengrunfeld <benjamin.grunfeld@gmail.com>
2018-05-03 18:07:24 +1000

    Enable webpack js, html and css loaders

--- vanillajs-app/src/index.js
+++ vanillajs-app/src/index.js
@@ -1,6 +1,8 @@
-const express = require('express')
+import express from 'express'
 
 const app = express()
+
+import style from './css/style.css'
 
 app.get('/', (req, res) => {
     res.status(200).send('Hello world').end()
--- vanillajs-app/webpack.config.js
+++ vanillajs-app/webpack.config.js
@@ -1,12 +1,45 @@
-const path = require('path')
+// Note that prod and dev mode are set in
+// the npm scripts
+
 const nodeExternals = require('webpack-node-externals')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = {
-  entry: './src/app.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      }
+    ]
   },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/html/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
   target: 'node',
   externals: [nodeExternals()],
   mode: 'development'
